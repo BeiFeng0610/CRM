5310d29c7160806f1f0f090c817bf0ff9b7dcf08
BeiFeng <1611606790@qq.com>
2020-07-28 03:52:17 +0800

    添加监听器，为服务器添加数据字典缓存

---
+++ crm/src/main/java/com/beifeng/crm/settings/dao/DicTypeDao.java
@@ -0,0 +1,12 @@
+package com.beifeng.crm.settings.dao;
+
+import com.beifeng.crm.settings.domain.DicType;
+
+import java.util.List;
+
+/**
+ * Author 北京动力节点
+ */
+public interface DicTypeDao {
+    List<DicType> getTypeList();
+}
